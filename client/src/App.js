753979c63c6d1c55b4be0b3407acc1e59d9d2b4b
Jana <jana.samson@hotmail.fr>
2022-11-19 20:59:36 -0500

    get access token with code

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 
 import Login from './components/Login'
+import Dashboard from './components/Dashboard'
 
 function App() {
+  const code = new URLSearchParams(window.location.search).get('code');
+
   return (
     <div className="App">
       <header></header>
       <main>
-        <Login />
+        {
+          code ? 
+          <Dashboard code={code} /> :
+          <Login />
+        }
       </main>
       <footer></footer>
     </div>
---
+++ client/src/components/Dashboard.jsx
@@ -0,0 +1,7 @@
+function Dashboard({ code }) {
+    return (
+        <div>dashboard</div>
+    )
+}
+
+export default Dashboard
